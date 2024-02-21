41b74242d2738bd7fab917adc5a45fc8b8b6d17e
Samuel <109113326+samuxka@users.noreply.github.com>
2024-02-21 18:51:57 -0300

    oian

---
+++ aula01.js
@@ -0,0 +1,18 @@
+// ECMAScript 2015
+
+// Declarando uma variavel
+// var, let e const Qual usar?
+// variaveis não podem começar com numero ou simbolos
+
+let minhaVariavel; // undefined
+let outraVariavel; // undefined
+
+minhaVariavel = "olá";
+outraVariavel = 100;
+
+// "var" escopo de função
+// "let" e "const" escopo de bloco
+
+var a = 10;
+let b = 10;
+const c = 10;
---
+++ aula02.js
@@ -0,0 +1,23 @@
+// Quase tudo em javascript é um objeto
+// Uma exeção são os tipos primitivos
+
+// tipos primitivos
+// são 5: number, string, boolean, undefined, null
+
+// ECMASCRIPT 6 (es2015) lançou um novo tipo primitivo chamado 'symbol'.
+// porém seu uso requer conhecimentos um pouco mais avançados.
+
+let meuNumero = 10;
+let minhaString = 'Olá Planeta!';
+let meuBooleano = true;
+let meuNull = null;
+let meuUndefined = undefined;
+
+console.log('Meu número é: ', meuNumero);
+
+console.log('O tipo do número é: ', typeof meuNumero);
+
+console.log('O tipo do número é: ', typeof meuNumero);
+console.log('O tipo da minha string é: ', typeof minhaString);
+console.log('O tipo do meu boolean é: ', typeof meuBooleano);
+console.log('O ')
